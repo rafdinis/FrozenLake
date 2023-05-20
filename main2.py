24280bc7661c2c014d0a3f2ae1a22732ceb23979
Sabeen Ashraf <Sabeen.ash01@gmail.com>
2023-05-20 15:40:53 +0100

    SelectionAlgorithms

--- main2.py
+++ main2.py
@@ -53,9 +53,9 @@ class GeneticAlgorithm:
                 self.evaluate_fitness(agent)
 
             # selection, crossover, and mutation operations
-            self.population = elitist_selection(self.population)
+            self.population = tournament_algorithm(self.population)
             self.population = Alternated_mosaic(self.population)
-            self.population = mutate_population(self.population)
+            self.population = standard_mutation(self.population)
 
         for agent in self.population:
             self.evaluate_fitness(agent)
